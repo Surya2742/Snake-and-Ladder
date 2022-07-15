ee30275d7ce0a3cedafb4381fbd1de0265a72372
Surya2742 <boby7724284@gmail.com>
2022-07-15 22:30:12 +0530

    UC2-Die Value 1 to 6 is added

---
+++ UC2.java
@@ -0,0 +1,11 @@
+
+public class UC2 {
+	
+	public static void main(String[] args) {
+		int startPosition = 0;
+	    System.out.println("Game start and Player is at Position " +startPosition);
+		int dice = (int) Math.floor(Math.random() * 6 + 1);
+		System.out.println("The Number on dice is " + dice);
+	}
+
+}

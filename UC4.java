67aed737b0e22b28e93c347477b50cf85d3d2e57
Surya2742 <boby7724284@gmail.com>
2022-07-16 22:58:26 +0530

    UC4-Player reaches at 100 by crossing all obstacles

---
+++ UC4.java
@@ -0,0 +1,40 @@
+
+public class UC4 {
+	
+	public static void main(String[] args) {   
+		System.out.println("Starting of Snake and Ladder Game");			
+	int LADDER = 1;
+	int SNAKE = 2;
+	int playerCurrentPosition = 0;
+			
+			while( playerCurrentPosition< 100) 
+			{
+			int dice = (int)((Math.random()*6) + 1 );
+			System.out.println("Dice number is:" +dice);   
+		    int position  = (int)(Math.random() * 3 );
+		    if(position == LADDER)
+		    {	
+		    	playerCurrentPosition += dice;
+		    if(playerCurrentPosition > 100)
+		    {
+		    	playerCurrentPosition -= dice;
+		    }
+		    System.out.println("Ladder moves forward "+ playerCurrentPosition);
+		    }
+		    else if(position == SNAKE)
+		    {	
+		    	playerCurrentPosition -= dice;
+		    if(playerCurrentPosition < 0)
+		    {
+		    	playerCurrentPosition = 0;
+		    }
+		    System.out.println("Snake moves backward "+ playerCurrentPosition);
+		    }
+		    else
+		    {
+		    System.out.println("Player is on "+ playerCurrentPosition);
+		    }
+		    
+	}
+}
+}
